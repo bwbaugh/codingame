f96cbb1450a433a5ee044f58e255596200eeb636
Wesley Baugh <wesley@bwbaugh.com>
2015-11-28 11:26:33 -0800

    Pick arbitrary human and stick with them

--- contests/codevszombies/main.py
+++ contests/codevszombies/main.py
@@ -1,21 +1,42 @@
-import sys
-import math
-
-# Save humans, destroy zombies!
+import collections
 
 
-# game loop
-while 1:
-    x, y = [int(i) for i in input().split()]
+Point = collections.namedtuple('Point', 'x y')
+
+
+def main():
+    while 1:
+        player = [int(x) for x in input().split()]
+        humans = _get_humans()
+        zombies = _get_zombies()
+        next_point = pick_next_point(player, humans, zombies)
+        print('{point.x} {point.y}'.format(point=next_point))
+
+
+def _get_humans():
+    humans = {}
     human_count = int(input())
-    for i in range(human_count):
-        human_id, human_x, human_y = [int(j) for j in input().split()]
+    for _ in range(human_count):
+        human_id, human_x, human_y = [int(x) for x in input().split()]
+        humans[human_id] = Point(x=human_x, y=human_y)
+    return humans
+
+
+def _get_zombies():
+    zombies = {'current': {}, 'future': {}}
     zombie_count = int(input())
     for i in range(zombie_count):
-        zombie_id, zombie_x, zombie_y, zombie_xnext, zombie_ynext = [int(j) for j in input().split()]
+        zombie_id, zombie_x, zombie_y, zombie_xnext, zombie_ynext = [
+            int(j) for j in input().split()
+        ]
+        zombies['current'][zombie_id] = Point(x=zombie_x, y=zombie_y)
+        zombies['future'][zombie_id] = Point(x=zombie_xnext, y=zombie_ynext)
+    return zombies
 
-    # Write an action using print
-    # To debug: print("Debug messages...", file=sys.stderr)
 
-    # Your destination coordinates
-    print("0 0")
+def pick_next_point(player, humans, zombies):
+    return next(iter(humans.values()))
+
+
+if __name__ == '__main__':
+    main()
