44650d113db89033a314e115bc6d2ed96c1e5c02
Wesley Baugh <wesley@bwbaugh.com>
2016-03-01 22:36:43 -0800

    Clean up auto generated code to be more idiomatic

--- contests/coders-strike-back/Main.hs
+++ contests/coders-strike-back/Main.hs
@@ -4,52 +4,19 @@ import Control.Monad
 main :: IO ()
 main = do
     hSetBuffering stdout NoBuffering -- DO NOT REMOVE
-    
-    -- Auto-generated code below aims at helping you parse
-    -- the standard input according to the problem statement.
-    
-    input_line <- getLine
-    let laps = read input_line :: Int
-    input_line <- getLine
-    let checkpointcount = read input_line :: Int
-    
-    replicateM checkpointcount $ do
-        input_line <- getLine
-        let input = words input_line
-        let checkpointx = read (input!!0) :: Int
-        let checkpointy = read (input!!1) :: Int
-        return ()
-    loop
 
-loop :: IO ()
-loop = do
-    
-    replicateM 2 $ do
-        input_line <- getLine
-        let input = words input_line
-        let x = read (input!!0) :: Int
-        let y = read (input!!1) :: Int
-        let vx = read (input!!2) :: Int
-        let vy = read (input!!3) :: Int
-        let angle = read (input!!4) :: Int
-        let nextcheckpointid = read (input!!5) :: Int
-        return ()
-    
-    replicateM 2 $ do
-        input_line <- getLine
-        let input = words input_line
-        let x = read (input!!0) :: Int
-        let y = read (input!!1) :: Int
-        let vx = read (input!!2) :: Int
-        let vy = read (input!!3) :: Int
-        let angle = read (input!!4) :: Int
-        let nextcheckpointid = read (input!!5) :: Int
-        return ()
-    
-    -- hPutStrLn stderr "Debug messages..."
-    
-    -- Write action to stdout
-    putStrLn "8000 4500 100"
-    putStrLn "8000 4500 100"
-    
-    loop
+    laps <- readLn :: IO Int
+    checkpointcount <- readLn :: IO Int
+    checkpoints <- replicateM checkpointcount $
+        fmap (map read . words) getLine :: IO [[Int]]
+
+    forever $ do
+        myPods <- replicateM 2 readPod
+        enemyPods <- replicateM 2 readPod
+
+        putStrLn "8000 4500 100"
+        putStrLn "8000 4500 100"
+
+-- | [x, y, vx, vy, angle, nextcheckpointid]
+readPod :: IO [Int]
+readPod = fmap (map read . words) getLine
