9254b7244319e94f4f520ddb9d9cfe2ad93ef8b6
Wesley Baugh <wesley@bwbaugh.com>
2016-03-03 22:47:05 -0800

    Define and use more types for readability

--- contests/coders-strike-back/Main.hs
+++ contests/coders-strike-back/Main.hs
@@ -2,11 +2,15 @@ import System.IO
 import Control.Monad
 
 data Pod = Pod {
-      getPos :: (Int, Int)
-    , getSpeed :: (Int, Int)
+      getPos :: Position
+    , getSpeed :: Position
     , getAngle :: Int
     , getCheckPoint :: Int
     } deriving (Show)
+
+type Checkpoint = Position
+type Position = (Int, Int)
+type Thrust = Int
 
 main :: IO ()
 main = do
@@ -14,14 +18,15 @@ main = do
 
     laps <- readLn :: IO Int
     checkpointcount <- readLn :: IO Int
-    checkpoints <- replicateM checkpointcount $
-        fmap (map read . words) getLine :: IO [[Int]]
+    checkpoints <- replicateM checkpointcount $ do
+        [x, y] <- fmap (map read . words) getLine
+        return (x, y)
 
     forever $ do
         myPods <- replicateM 2 readPod
         enemyPods <- replicateM 2 readPod
         forM_ myPods $ \pod -> do
-            let (x, y, thrust) = pickMove checkpoints pod
+            let ((x, y), thrust) = pickMove checkpoints pod
             putStrLn . unwords . map show $ [x, y, thrust]
 
 readPod :: IO Pod
@@ -29,8 +34,8 @@ readPod = do
     [x, y, vx, vy, angle, nextCheckPointId] <- fmap (map read . words) getLine
     return $ Pod (x, y) (vx, vy) angle nextCheckPointId
 
-pickMove :: [[Int]] -> Pod -> (Int, Int, Int)
-pickMove checkpoints pod = (x, y, thrust)
+pickMove :: [Checkpoint] -> Pod -> (Position, Thrust)
+pickMove checkpoints pod = (position, thrust)
   where
-    [x, y] = checkpoints !! getCheckPoint pod
+    position = checkpoints !! getCheckPoint pod
     thrust = 100
