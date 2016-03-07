7493750b7bd3cc0d0ee1afed560a8e76a99125c9
Wesley Baugh <wesley@bwbaugh.com>
2016-03-06 22:25:34 -0800

    Implement next state

--- community/game-of-life/src/Main.hs
+++ community/game-of-life/src/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
-data Cell = Dead | Live deriving (Show)
+import Data.List.Split (chunksOf)
+
+data Cell = Dead | Live deriving (Eq, Show)
 newtype Grid = Grid [[Cell]]
 
 instance Show Grid where
@@ -21,4 +23,43 @@ parseCell '1' = Live
 parseCell x = error $ "parseCell: unknown char: " ++ [x]
 
 nextState :: Grid -> Grid
-nextState = undefined
+nextState grid@(Grid xss) = Grid . chunksOf numCols . map (uncurry applyRule) $ zip (concat xss) neighbors
+  where
+    neighbors :: [Int]
+    neighbors = map (flip countLiveNeighbors grid) indices
+    indices :: [(Int, Int)]
+    indices = map toPoint . sequence $ [[0..numRows-1], [0..numCols-1]]
+    (numRows, numCols) = getDimensions grid
+
+getDimensions :: Grid -> (Int, Int)
+getDimensions (Grid xss) = (numRows, numCols)
+  where
+    numRows = length xss
+    numCols = length $ head xss
+
+toPoint :: [Int] -> (Int, Int)
+toPoint [r, c] = (r, c)
+toPoint _ = error "toPoint non-exhaustive"
+
+countLiveNeighbors :: (Int, Int) -> Grid -> Int
+countLiveNeighbors pos grid = length . filter (== Live) $ getNeighbors pos grid
+
+getNeighbors :: (Int, Int) -> Grid -> [Cell]
+getNeighbors (r, c) (Grid xss) = map getCell . filter inBounds $ filter (/= (r, c)) toTry
+  where
+    toTry :: [(Int, Int)]
+    toTry = map toPoint . sequence $ [[r-1, r, r+1], [c-1, c, c+1]]
+    inBounds (r', c')
+        | r' < 0 || c' < 0 || r' >= numRows || c' >= numCols = False
+        | otherwise = True
+      where
+        (numRows, numCols) = getDimensions $ Grid xss
+    getCell :: (Int, Int) -> Cell
+    getCell (r', c') = xss !! r' !! c'
+
+applyRule :: Cell -> Int -> Cell
+applyRule Live 2 = Live
+applyRule Live 3 = Live
+applyRule Live _ = Dead
+applyRule Dead 3 = Live
+applyRule Dead _ = Dead
