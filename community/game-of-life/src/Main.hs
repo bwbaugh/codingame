4d33f3b701d4746d5c88e302b988b5e5d5568a0d
Wesley Baugh <wesley@bwbaugh.com>
2016-03-06 20:39:55 -0800

    Parse cells and output unchanged

--- community/game-of-life/src/Main.hs
+++ community/game-of-life/src/Main.hs
@@ -1,5 +1,14 @@
 module Main where
 
+data Cell = Dead | Live
+
+instance Show Cell where
+    show Dead = "0"
+    show Live = "1"
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = interact $ unlines . map (concatMap (show . parseGrid)) . tail . lines
+
+parseGrid :: Char -> Cell
+parseGrid '0' = Dead
+parseGrid '1' = Live
