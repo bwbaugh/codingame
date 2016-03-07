14d7c9d0ed9d68176672832395104be7638b6f9e
Wesley Baugh <wesley@bwbaugh.com>
2016-03-06 20:51:05 -0800

    Parse grid

--- community/game-of-life/src/Main.hs
+++ community/game-of-life/src/Main.hs
@@ -1,14 +1,19 @@
 module Main where
 
 data Cell = Dead | Live
+type Grid = [[Cell]]
 
 instance Show Cell where
     show Dead = "0"
     show Live = "1"
 
 main :: IO ()
-main = interact $ unlines . map (concatMap (show . parseGrid)) . tail . lines
+main = interact $ unlines . map (concatMap show) . parseGrid . tail . lines
 
-parseGrid :: Char -> Cell
-parseGrid '0' = Dead
-parseGrid '1' = Live
+parseGrid :: [String] -> Grid
+parseGrid = map (map parseCell)
+
+parseCell :: Char -> Cell
+parseCell '0' = Dead
+parseCell '1' = Live
+parseCell x = error $ "parseCell: unknown char: " ++ [x]
