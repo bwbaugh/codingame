8660233a61785239e72874dbc2a919159f971a72
Wesley Baugh <wesley@bwbaugh.com>
2016-03-16 22:18:08 -0700

    Sketch out initial design and data types

--- community/haunted-manor/src/Main.hs
+++ community/haunted-manor/src/Main.hs
@@ -1,53 +1,57 @@
 module Main where
 
-import System.IO
 import Control.Monad
+
+type Manor = [[Maybe Cell]]
+type Cell = Either Mirror Monster
+data Mirror = DiagonalDown | DiagonalUp deriving (Show)
+data Monster = Vampire | Zombie | Ghost deriving (Show)
+data Count = Count {
+      numVampire :: Int
+    , numZombie :: Int
+    , numGhost :: Int
+    } deriving (Show)
+data Seen = Seen {
+      seenTop :: [Int]
+    , seenBot :: [Int]
+    , seenLeft :: [Int]
+    , seenRight :: [Int]
+    } deriving (Show)
 
 main :: IO ()
 main = do
-    hSetBuffering stdout NoBuffering -- DO NOT REMOVE
-    
-    -- Auto-generated code below aims at helping you parse
-    -- the standard input according to the problem statement.
-    
-    input_line <- getLine
-    let input = words input_line
-    let vampirecount = read (input!!0) :: Int
-    let zombiecount = read (input!!1) :: Int
-    let ghostcount = read (input!!2) :: Int
-    input_line <- getLine
-    let size = read input_line :: Int
-    input_line <- getLine
-    let input = words input_line
-    
-    forM [0..(size-1)] $ \i -> do
-        let canseefromtop = read (input!!(i)) :: Int
-        return ()
-    input_line <- getLine
-    let input = words input_line
-    
-    forM [0..(size-1)] $ \i -> do
-        let canseefrombottom = read (input!!(i)) :: Int
-        return ()
-    input_line <- getLine
-    let input = words input_line
-    
-    forM [0..(size-1)] $ \i -> do
-        let canseefromleft = read (input!!(i)) :: Int
-        return ()
-    input_line <- getLine
-    let input = words input_line
-    
-    forM [0..(size-1)] $ \i -> do
-        let canseefromright = read (input!!(i)) :: Int
-        return ()
-    
-    replicateM size $ do
-        row <- getLine
-        return ()
-    
-    -- hPutStrLn stderr "Debug messages..."
-    
-    -- Write answer to stdout
-    putStrLn "answer"
-    return ()
+    [vampire, zombie, ghost] <- readWSV :: IO [Int]
+    let count = Count vampire zombie ghost
+    size <- readLn
+    [top, bot, left, right] <- replicateM 4 readWSV :: IO [[Int]]
+    let seen = Seen top bot left right
+    manor <- liftM parseManor (replicateM size getLine)
+    case possibleSolutions manor count seen of
+        [] -> putStrLn "NONE"
+        (x:_) -> putStr $ showManor x
+  where
+    -- | Whitespace Separated Values.
+    readWSV :: Read a => IO [a]
+    readWSV = fmap (map read . words) getLine
+
+parseManor :: [String] -> Manor
+parseManor = map (map parseCell)
+
+parseCell :: Char -> Maybe Cell
+parseCell '.' = Nothing
+parseCell '\\' = Just (Left DiagonalDown)
+parseCell '/' = Just (Left DiagonalUp)
+parseCell x = error $ "unexpected input grid char: " ++ [x]
+
+showManor :: Manor -> String
+showManor = unlines . map (concatMap (maybe "." showCell))
+
+showCell :: Cell -> String
+showCell (Left DiagonalDown) = "\\"
+showCell (Left DiagonalUp) = "/"
+showCell (Right Vampire) = "V"
+showCell (Right Zombie) = "Z"
+showCell (Right Ghost) = "G"
+
+possibleSolutions :: Manor -> Count -> Seen -> [Manor]
+possibleSolutions = undefined
