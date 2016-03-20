4d68a83f87b86ffe0fa7d5ad29fa595ddee52a91
Wesley Baugh <wesley@bwbaugh.com>
2016-03-19 18:53:19 -0700

    Index vector as if two dimensional

--- community/divine/src/Main.hs
+++ community/divine/src/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Control.Monad
 import Data.Char (digitToInt)
+import Data.Vector.Unboxed ((!))
 import qualified Data.Vector.Unboxed as U
 
 type Grid = U.Vector Int
@@ -16,3 +17,8 @@ readGrid = liftM parseGrid (replicateM 9 getLine)
 
 parseGrid :: [String] -> Grid
 parseGrid = U.fromList . map digitToInt . concat . words . unlines
+
+getCell :: Grid -> (Int, Int) -> Int
+getCell grid (row, column) = grid ! idx
+  where
+    idx = row * 9 + column
