915880eae32d966214cacc3a049c09930ee7a975
Wesley Baugh <wesley@bwbaugh.com>
2016-03-20 14:32:30 -0700

    Sketch out main function

--- community/SpreadsheetLabels.hs
+++ community/SpreadsheetLabels.hs
@@ -27,22 +27,8 @@ Input
 Output
 A Z AA AZ BA ZZ
 -} 
-import System.IO
-import Control.Monad
-
 main :: IO ()
-main = do
-    hSetBuffering stdout NoBuffering -- DO NOT REMOVE
-    
-    -- Auto-generated code below aims at helping you parse
-    -- the standard input according to the problem statement.
-    
-    input_line <- getLine
-    let n = read input_line :: Int
-    input_line <- getLine
-    let input = words input_line
-    
-    forM [0..(n-1)] $ \i -> do
-        let label = input!!(i)
-        return ()
-    return ()
+main = interact $ unwords . map convert . tail . words
+
+convert :: String -> String
+convert = undefined
