d361021dc530b409f85ea3183240d0843f246434
Wesley Baugh <wesley@bwbaugh.com>
2016-05-27 18:16:30 -0700

    Implement triforce

--- community/triforce/Triforce.hs
+++ community/triforce/Triforce.hs
@@ -2,17 +2,29 @@ import System.IO
 import Control.Monad
 
 main :: IO ()
-main = do
-    hSetBuffering stdout NoBuffering -- DO NOT REMOVE
-    
-    -- Auto-generated code below aims at helping you parse
-    -- the standard input according to the problem statement.
-    
-    input_line <- getLine
-    let n = read input_line :: Int
-    
-    -- hPutStrLn stderr "Debug messages..."
-    
-    -- Write answer to stdout
-    putStrLn "answer"
-    return ()
+main = readLn >>= putStr . unlines . fixWithPeriod . triforce
+  where
+    fixWithPeriod ((' ':xs):xxs) = ('.':xs):xxs
+
+triforce :: Int -> [String]
+triforce n = map (leftPad n) (triangle n) ++ secondRow n
+
+triangle :: Int -> [String]
+triangle n = zipWith (++) (pads n) stars
+  where
+    stars = map (`replicate` '*') $ take n [1,3..]
+
+pads :: Int -> [String]
+pads n = map (padForRow n) [1..n]
+
+padForRow :: Int -> Int -> String
+padForRow n k = replicate (n - k) ' '
+
+leftPad :: Int -> String -> String
+leftPad n x = replicate n ' ' ++ x
+
+secondRow :: Int -> [String]
+secondRow n = zipWith (++) t s
+  where
+    t = triangle n
+    s = zipWith (++) (pads (n + 1)) t
