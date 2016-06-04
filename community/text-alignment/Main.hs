48af41deee8a26293260c747657fb5a38f1e565f
Wesley Baugh <wesley@bwbaugh.com>
2016-06-03 18:07:35 -0700

    Left alignment

--- community/text-alignment/Main.hs
+++ community/text-alignment/Main.hs
@@ -1,23 +1,32 @@
-import System.IO
-import Control.Monad
+import Control.Monad (replicateM)
+
+data Alignment =
+    -- | Display text as-is.
+      LEFT
+    -- | Align text to the right.
+    | RIGHT
+    -- | Spread the remaining space evenly on both sides of the text.
+    -- If the number of remaining spaces is odd, leave the extra space
+    -- on the right. Don't actually append space characters to the end
+    -- of the line, though.
+    | CENTER
+    -- | Spread the remaining space evenly between words of the line so
+    -- that all lines end on the same column, with the exception that
+    -- lines with a single word should remain left-aligned. When
+    -- necessary, round the number of spaces to their integer part. For
+    -- instance if you need to spread 11 spaces between 5 words, the
+    -- theoretical number of spaces between each pair of words is 2.75,
+    -- yielding a total number of used spaces of 2.75 - 5.5 - 8.75 -
+    -- 11. Round to 2 - 5 - 8 - 11 and the effective number of spaces
+    -- in each block is 2 - 3 - 3 - 3.
+    | JUSTIFY
+    deriving (Show, Read)
 
 main :: IO ()
 main = do
-    hSetBuffering stdout NoBuffering -- DO NOT REMOVE
-    
-    -- Auto-generated code below aims at helping you parse
-    -- the standard input according to the problem statement.
-    
-    alignment <- getLine
-    input_line <- getLine
-    let n = read input_line :: Int
-    
-    replicateM n $ do
-        text <- getLine
-        return ()
-    
-    -- hPutStrLn stderr "Debug messages..."
-    
-    -- Write answer to stdout
-    putStrLn "answer"
-    return ()
+    alignment <- readLn
+    xs <- readLn >>= flip replicateM getLine
+    putStr . unlines $ align alignment xs
+
+align :: Alignment -> [String] -> [String]
+align _ xs = xs
