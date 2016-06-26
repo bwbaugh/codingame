a6af1174e631e37237bea529bbbb0ab15a9a37b9
Wesley Baugh <wesley@bwbaugh.com>
2016-06-25 23:42:52 -0700

    Clean up auto generated code with data types

--- contests/codebusters/Main.hs
+++ contests/codebusters/Main.hs
@@ -1,42 +1,86 @@
-import System.IO
-import Control.Monad
+{-# OPTIONS_GHC -Wall #-}
+module Main where
 
+import Control.Applicative ((<$>), (<*>))
+import Control.Monad (replicateM, replicateM_)
+import System.IO (BufferMode(NoBuffering), hSetBuffering, stdout)
+
+data Base = TopLeft | BotRight deriving (Show)
+
+data InitialState = InitialState
+    { bustersPerPlayer :: !Int  -- ^ The amount of busters you control.
+    , ghostCount :: !Int -- ^ The amount of ghosts on the map.
+    , myBase :: !Base -- ^ The base for the team you are on.
+    } deriving (Show)
+
+data Entity state team = Entity
+    { eId :: !Int  -- ^ Buster-ID or Ghost-ID.
+    , ePos :: !(Int, Int)  -- ^ An (x, y) point.
+    , eState :: !state
+    , eTeam :: !team
+    } deriving (Show)
+
+newtype BusterState = CarryingGhost (Maybe GhostId) deriving (Show)
+
+type GhostId = Int
+
+newtype GhostState = BustersTrapping Int deriving (Show)
+
+type Buster = Entity BusterState Base
+
+type Ghost = Entity GhostState ()
+
+-- | Wrapper to allow returning one of these types.
+data AnEntity = ABuster Buster | AGhost Ghost deriving (Show)
+
+data Move = MOVE Int Int | BUST Int | RELEASE deriving (Show)
+
+-- | Send your busters out into the fog to trap ghosts and bring them home!
 main :: IO ()
-main = do
-    hSetBuffering stdout NoBuffering -- DO NOT REMOVE
-    
-    -- Send your busters out into the fog to trap ghosts and bring them home!
-    
-    input_line <- getLine
-    let bustersperplayer = read input_line :: Int -- the amount of busters you control
-    input_line <- getLine
-    let ghostcount = read input_line :: Int -- the amount of ghosts on the map
-    input_line <- getLine
-    let myteamid = read input_line :: Int -- if this is 0, your base is on the top left of the map, if it is one, on the bottom right
-    loop bustersperplayer
+main = hSetBuffering stdout NoBuffering >> readInitialState >>= loop
 
-loop :: Int -> IO ()
-loop bustersperplayer = do
-    input_line <- getLine
-    let entities = read input_line :: Int -- the number of busters and ghosts visible to you
-    
-    replicateM entities $ do
-        input_line <- getLine
-        let input = words input_line
-        let entityid = read (input!!0) :: Int -- buster id or ghost id
-        let x = read (input!!1) :: Int
-        let y = read (input!!2) :: Int -- position of this buster / ghost
-        let entitytype = read (input!!4) :: Int -- the team id if it is a buster, -1 if it is a ghost.
-        let state = read (input!!4) :: Int -- For busters: 0=idle, 1=carrying a ghost.
-        let value = read (input!!5) :: Int -- For busters: Ghost id being carried. For ghosts: number of busters attempting to trap this ghost.
-        return ()
-    
-    replicateM bustersperplayer $ do
-        
-        -- hPutStrLn stderr "Debug messages..."
-        
-        -- MOVE x y | BUST id | RELEASE
-        putStrLn ("MOVE 8000 4500")
-        return ()
-    
-    loop bustersperplayer
+readInitialState :: IO InitialState
+readInitialState = InitialState <$> readLn <*> readLn <*> (readBase <$> readLn)
+
+readBase :: Int -> Base
+readBase 0 = TopLeft
+readBase 1 = BotRight
+readBase x = error $ "Unknown team-ID: " ++ show x
+
+loop :: InitialState -> IO ()
+loop initialState = do
+    _ <- readLn >>= flip replicateM readEntity
+    replicateM_ (bustersPerPlayer initialState) $
+        print move
+    loop initialState
+
+readEntity :: IO AnEntity
+readEntity = do
+    [entityId, x, y, entityType', _, value] <- map read . words <$> getLine
+    let parseBuster' base = ABuster $ parseBuster entityId (x, y) base value
+        in case entityType' of
+            0 -> return $ parseBuster' TopLeft
+            1 -> return $ parseBuster' BotRight
+            (-1) -> return $ AGhost (parseGhost entityId (x, y) value)
+            t -> error $ "Unknown entity type: " ++ show t
+
+parseBuster :: Int -> (Int, Int) -> Base -> Int -> Buster
+parseBuster entityId pos base value = Entity
+    { eId = entityId
+    , ePos = pos
+    , eState = CarryingGhost $ case value of
+        (-1) -> Nothing
+        x -> Just x
+    , eTeam = base
+    }
+
+parseGhost :: Int -> (Int, Int) -> Int -> Ghost
+parseGhost entityId pos value = Entity
+    { eId = entityId
+    , ePos = pos
+    , eState = BustersTrapping value
+    , eTeam = ()
+    }
+
+move :: Move
+move = MOVE 8000 4500
